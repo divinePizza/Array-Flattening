36b0854842f4a7a4415faa652c3bc6b9253ead77
divinePizza <tejas.mangalpally@gmail.com>
2024-10-27 14:19:14 +0530

    Add files via upload

--- flatten.js
+++ flatten.js
@@ -1,5 +1,9 @@
-function flattenArray(arr) {
+function flatten(arr) {
     return arr.reduce((acc, val) => {
-        return acc.concat(Array.isArray(val) ? flattenArray(val) : val);
+        return acc.concat(Array.isArray(val) ? flatten(val) : val);
     }, []);
-}
+  }
+  
+  const arr = [1, [2, [3, 4]]];
+  const flattened = flatten(arr);
+  console.log(flattened); 
